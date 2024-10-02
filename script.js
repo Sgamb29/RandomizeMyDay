63267cded83feb2e450152d216305449c6fb740e
Sgamb29 <shagamb@gmail.com>
2024-10-02 17:13:46 -0600

    added save cookie

--- script.js
+++ script.js
@@ -23,8 +23,23 @@ function handleClick(el) {
         return;
     }
     el.value = "";
+    addDeleteButton(el);
+
     addNewTextArea();
+    document.getElementById("saveButton").innerText = "Save List";
 } 
+
+function addDeleteButton(el) {
+    const delBtn = document.createElement("button");
+    delBtn.style.backgroundColor = "pink";
+    delBtn.className = "deleteButton";
+    delBtn.addEventListener("click", () => {
+        document.getElementById("inputs").removeChild(el);
+        document.getElementById("inputs").removeChild(delBtn);
+    });
+    delBtn.innerText = "Remove";
+    document.getElementById("inputs").appendChild(delBtn);
+}
 
 
 function addNewTextArea() {
@@ -63,7 +78,7 @@ function randomize() {
     for (let i = 0; i < shuffledList.length; i++) {
         currentList[i].value = shuffledList[i];
     }
-
+    document.getElementById("saveButton").innerText = "Save List";
     // Hack for making button animation run every press by removing the focus
     setTimeout(() => {
         document.getElementById("dummyButton").focus();
@@ -73,4 +88,91 @@ function randomize() {
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
+}
+
+
+// Cookie Logic
+
+function getCookie(name) {
+    const value = document.cookie.split(`${name}=`)[1];
+    const listStrings = value.split(",");
+    return listStrings;    
+}
+
+function setCookie(name, value, days) {
+    const date = new Date();
+    date.setTime(date.getTime() + (days*24*60*60*1000));
+    const expires = `expires=${date.toUTCString()}`;
+    document.cookie = `${name}=${value};${expires};path=/`;
+}
+
+function saveList() {
+
+    const currentElements = document.getElementsByClassName("activityInput");
+    const currentList = Array.from(currentElements);
+    let listStrings = [];
+    currentList.forEach((el) => {
+        if (el.value == "+" | el.value === "") {
+            return;
+        }
+        listStrings.push(el.value.replaceAll(",", "commaEncode1"));
+    });
+    setCookie('currentList', listStrings, 365);
+    document.getElementById("saveButton").innerText = "Saved"
+}
+
+// Run the main function on page load
+document.addEventListener('DOMContentLoaded', () => {
+    generateSavedList();
+});
+
+
+
+function generateSavedList() {
+    const listStrings = getCookie("currentList");
+    if (listStrings.length < 1) {
+        return;
+    }
+    if (listStrings[0] === "") {
+        return;
+    }
+
+    let activtyElements = document.getElementsByClassName("activityInput");
+    let activityElList = Array.from(activtyElements);
+    let currentAdderElement = activityElList[0];
+    currentAdderElement.value = listStrings[0].replaceAll("commaEncode1", ",");
+    addDeleteButton(currentAdderElement);
+
+    for (let i = 1; i < listStrings.length; i++) {
+        const newEl = document.createElement("textarea");
+        newEl.classList.add("activityInput");        
+        newEl.value = listStrings[i].replaceAll("commaEncode1", ",");
+        document.getElementById("inputs").appendChild(newEl); 
+        newEl.addEventListener("click", () => {
+            handleClick(newEl);
+        });
+        addDeleteButton(newEl);
+    }
+
+    addNewTextArea();  
+    document.getElementById("saveButton").innerText = "Saved"; 
+}
+
+function removeSave() {
+    setCookie("currentList", "", 365);
+    document.getElementById("saveButton").innerText = "Save List"; 
+    document.getElementById("removeSaveButton").innerText = "Save Removed";
+}
+
+function clearAll() {
+    let deleteButtons = document.getElementsByClassName("deleteButton");
+    let deleteElList = Array.from(deleteButtons);
+
+    deleteElList.forEach((el) => {
+        el.click();
+    })
+
+    document.getElementById("saveButton").innerText = "Save List"; 
+
+
 }
